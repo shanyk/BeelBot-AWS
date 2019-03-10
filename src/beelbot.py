ec3e6f5bcc4182d2dc2e361f4d64537e7675882f
shanyk <35853225+shanyk@users.noreply.github.com>
2019-03-10 01:05:12 -0500

    adding code to start up bot

---
+++ src/beelbot.py
@@ -0,0 +1,15 @@
+import discord
+import asyncio
+from discord.ext import commands 
+
+# create discord bot 
+bot = commands.Bot(command_prefix="$")
+
+# get bot token from config.txt
+botToken = None 
+with open('config.txt', 'r') as f:
+    botToken = f.readline().strip()
+
+@bot.event 
+async def on_ready():
+    print('Beel is online.')
