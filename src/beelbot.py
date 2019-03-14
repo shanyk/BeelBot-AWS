b6286911f458b76f48fef60fc51334a265d7d1cd
Yuankai Shan <shan.yuankai@gmail.com>
2019-03-13 22:04:37 -0400

    fixed dependency to discord.py rewrite, installed boto3, added command to take bot offline

--- src/beelbot.py
+++ src/beelbot.py
@@ -3,7 +3,8 @@ import asyncio
 from discord.ext import commands 
 
 # create discord bot 
-bot = commands.Bot(command_prefix="$")
+prefix = "$"
+bot = commands.Bot(command_prefix=prefix)
 
 # get bot token from config.txt
 botToken = None 
@@ -13,5 +14,37 @@ with open('config.txt', 'r') as f:
 @bot.event 
 async def on_ready():
     print('Beel is online.')
+
+# @bot.command()
+# async def test(ctx, arg1, arg2):
+#     print(arg1, arg2)
+#     await ctx.send(arg1, arg2)
+
+@bot.command()
+async def ping(ctx):
+    '''
+    This text will be shown in the help command
+    '''
+
+    # Get the latency of the bot
+    latency = bot.latency  # Included in the Discord.py library
+    # Send it to the user
+    print(ctx)
+    await ctx.send(latency)
+
+@bot.command()
+async def offline(ctx):	
+	'''
+		Admin command to take the bot offline
+
+		Arguments:
+		ctx -- message object read by the bot
+	'''
+	if 'Admin' in [role.name for role in ctx.author.roles]:	
+		await ctx.send('Going offline...')
+		await bot.close()
+	else:
+		await ctx.send('Nice try')
+		return
 
 bot.run(botToken)
